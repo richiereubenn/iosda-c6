6786a27c8d977719280d44a455ef61c2b47285a2
Richie Reuben Hermanto <richiereubenh@gmail.com>
2025-09-01 01:54:28 +0700

    fix: responsive unitcomplain card

---
+++ iosda-c6-frontend/Views/BIComplainDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  BIComplainDetailView.swift
+//  iosda-c6-frontend
+//
+//  Created by Richie Reuben Hermanto on 01/09/25.
+//
+
+import SwiftUI
+
+struct BIComplainDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BIComplainDetailView()
+}
